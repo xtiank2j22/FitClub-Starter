cf17b1d807801c9a37bd1bd967901f10a82e5687
Your Namextiank2j22 <xtiank2j@gmail.com>
2025-06-09 04:22:46 +0100

    first reactjs design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+import "./App.css";
+import Hero from './components/Hero/Hero';
+import Programs from "./components/Programs/Programs";
+import Reasons from "./components/Reasons/Reasons";
+
+
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+    {/* the hero section */}
+      <Hero />
+      {/* program section */}
+      <Programs/>
+      {/* section for the reasons */}
+      <Reasons/>
     </div>
   );
 }
